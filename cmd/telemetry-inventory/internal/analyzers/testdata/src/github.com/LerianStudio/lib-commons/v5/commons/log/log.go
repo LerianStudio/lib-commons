e0d469b730fb75af0a7eb2c357ef3c15c48a3e79
Fred Amaral <fred@fredamaral.com.br>
2026-05-11 17:11:33 -0400

    chore(telemetry-inventory): trim unused field constructors from log testdata stub
    Bool, Err, and Any field constructors are unreferenced across all
    fixtures under testdata/src/. Only String and Int are exercised by
    the logfield fixture. Drop the dead declarations.

--- cmd/telemetry-inventory/internal/analyzers/testdata/src/github.com/LerianStudio/lib-commons/v5/commons/log/log.go
+++ cmd/telemetry-inventory/internal/analyzers/testdata/src/github.com/LerianStudio/lib-commons/v5/commons/log/log.go
@@ -28,6 +28,3 @@ type Logger interface {
 
 func String(string, string) Field { return Field{} }
 func Int(string, int) Field       { return Field{} }
-func Bool(string, bool) Field     { return Field{} }
-func Err(error) Field             { return Field{} }
-func Any(string, any) Field       { return Field{} }
